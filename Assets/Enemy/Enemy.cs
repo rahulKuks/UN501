07e2ff99ace534003ff482dd736f2b9fffb0fb08
Rahul Kukreja <rkuk591@gmail.com>
2019-05-25 11:58:03 +0800

    UN501 project committed

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+    private Player player;
+    public float speed;
+    public float attackForce = 10f;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindObjectOfType<Player>();
+        if(player == null)
+        {
+            Debug.LogError("Cannot find player! Error!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.LookAt(player.transform);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody rigidbody = player.GetComponent<Rigidbody>();
+            rigidbody.AddForce(attackForce * transform.forward, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Terrain/Player/Player.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 3;
+    public Rigidbody rigidbody;
+    public float jumpForce = 5;
+    public bool isGrounded;
+    public Color rayColor;
+    public Camera eyes;
+
+    public float lookHorizontal = 0f;
+    public float lookVertical;
+
+    public GameObject bluePortalPrefab;
+    public GameObject orangePortalPrefab;
+
+   
+
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + (transform.right * -1 * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position +(transform.right * Time.deltaTime * speed);
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + (transform.forward * Time.deltaTime * speed);
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + (transform.forward * Time.deltaTime * speed);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        }
+
+        lookHorizontal += Input.GetAxis("Mouse X");
+        lookVertical += Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(-lookVertical, lookHorizontal, 0f);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray();
+            ray.origin = eyes.transform.position;
+            ray.direction = eyes.transform.forward;
+            Debug.DrawRay(ray.origin, ray.direction * 100f, rayColor, 10f);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.LogError(" Ray hit! Object: " + hit.transform.name + " Position: " + hit.point);
+                Quaternion portalRotation = Quaternion.LookRotation(this.transform.position - hit.point, hit.normal);
+                     //Quaternion.LookRotation(hit.normal, Vector3.up);
+                Instantiate(orangePortalPrefab, hit.point, portalRotation);
+            }
+
+
+        }
+    }
+}
